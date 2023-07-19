c424d311d8ec0aa402bcd86b48cf1edb76777adf
HoangPhucPc <hoangphuc311tn@gmail.com>
2023-07-19 12:47:50 +0700

    push

--- src/Baitapthuchanhlayout/Gallery.jsx
+++ src/Baitapthuchanhlayout/Gallery.jsx
@@ -4,18 +4,20 @@ const Gallery = () => {
   return (
     <div className='container mt-4'>
       <div className="row gx-lg-5">
-        <div className="col-4">
-          <div className="item text-center bg-light p-4">
-            <div className="item_icon"><i class="bi bi-collection"></i></div>
+        <div className="col-4 mb-5">
+          <div className="item text-center bg-light p-5 h-100">
+            <div className="item_icon bg-primary text-white">
+            <i class="fa-solid fa-ethernet"></i>
+              </div>
             <h2 className='fs-4 fw-bold'>Fresh new layout</h2>
             <p className='mb-0'>With Bootstrap 5, we've created a fresh new layout for this template!</p>
           </div>
         </div>
-        <div className="col-4">M</div>
-        <div className="col-4">M</div>
-        <div className="col-4">M</div>
-        <div className="col-4">M</div>
-        <div className="col-4">M</div>
+        <div className="col-4 mb-5"></div>
+        <div className="col-4 mb-5"></div>
+        <div className="col-4 mb-5"></div>
+        <div className="col-4 mb-5"></div>
+        <div className="col-4 mb-5"></div>
       </div>
     </div>
   )
